830e858ba68c2c37ec93a34bc1c895e842077e7e
happyhappyyay <chapan.denis@gmail.com>
2020-08-18 21:16:14 -0500

    removed duplicate instructions

--- src/component/Introduction/Introduction.jsx
+++ src/component/Introduction/Introduction.jsx
@@ -11,12 +11,11 @@ const Introduction = () => {
                 <br></br> <br></br>
                 You will see a series of silhouettes denoting islanders. Your task is to categorize each of these islanders as best you can
                 by placing them at either island 1 or island 2 (names have been removed for the sake of anonymity). You can either click the island or press the corresponding
-                numerical key (e.g., 1 or 2). You will also be asked to give your idea of what a representative member of each island would be.
+                numerical key (e.g., "1" or "2"). You will also be asked to give your idea of what a representative member of each island would be.
                 <br></br><br></br>
                 
                 Select the <mark>amount of feedback</mark> (how often you want to know if your answer was correct) and <mark>randomization of feedback </mark>
-                (mixing up true and false responses to your answers). Start with a training session where you get feedback. Click the island you think is correct
-                 or press a corresponding "1" or "2". Then, embark on a journey of island categorization with your selected amount and type of feedback! 
+                (mixing up true and false responses to your answers). Start with a training session where you get feedback. Then, embark on a journey of island categorization with your selected amount and type of feedback! 
                  Make it all the way through and you can get your results at the end!
                 <br></br><br></br><b>No information of any kind is being collected.</b>
 
