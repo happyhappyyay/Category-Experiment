ce49ab8e30653095ae32f9081208a9040e61aab1
happyhappyyay <chapan.denis@gmail.com>
2020-08-18 21:18:35 -0500

    Updates

---
+++ precache-manifest.0e76738ce27dab8fe6a7708bb2ad6c05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db2b49b811fbacda89cd92ce723ad4d",
+    "url": "/Category-Experiment/index.html"
+  },
+  {
+    "revision": "faec89a0daa918239b21",
+    "url": "/Category-Experiment/static/css/main.ce3783ce.chunk.css"
+  },
+  {
+    "revision": "f6522afda969299cd2b5",
+    "url": "/Category-Experiment/static/js/2.e2cd6110.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Category-Experiment/static/js/2.e2cd6110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faec89a0daa918239b21",
+    "url": "/Category-Experiment/static/js/main.6b51d94d.chunk.js"
+  },
+  {
+    "revision": "b2d5831d2c52476ad252",
+    "url": "/Category-Experiment/static/js/runtime-main.0d38d131.js"
+  },
+  {
+    "revision": "a3b286a697d55c12dd05466a46c1d201",
+    "url": "/Category-Experiment/static/media/silhouette-add.a3b286a6.svg"
+  },
+  {
+    "revision": "db51946671aa8a8c91ec717d26de6ab6",
+    "url": "/Category-Experiment/static/media/silhouette-remove.db519466.svg"
+  },
+  {
+    "revision": "b7f928cd408ebb4017471ef5da2b0ddd",
+    "url": "/Category-Experiment/static/media/silhouette.b7f928cd.svg"
+  }
+]);
